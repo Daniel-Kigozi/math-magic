b2f694e7f8918c1ebc7f9d27b54d27a99bd33267
Daniel-Kigozi <kdanalex69@gmail.com>
2023-04-29 16:15:04 +0300

    put quotes bellow calculator

--- src/components/Quotation/quotes.js
+++ src/components/Quotation/quotes.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import Image from './Loading.gif';
 import './quote.css';
 
 const MyQuotation = () => {
   const [myQuotation, setMyQuotation] = useState('');
   const [authorOfquote, setAuthorOfquote] = useState('');
+  const [quoteLoading, setLoadingQuote] = useState(true);
   const [quoteError, setQuoteError] = useState('');
   useEffect(() => {
     const fetchQuotes = async () => {
@@ -19,18 +21,20 @@ const MyQuotation = () => {
         const data = await response.json();
         setMyQuotation(data[0].quote);
         setAuthorOfquote(data[0].author);
+        setLoadingQuote(false);
       } catch (error) {
         setQuoteError('');
       }
     };
     setTimeout(() => {
       fetchQuotes();
-    }, 2000);
+    }, 1000);
   }, []);
   return (
     <>
       <center>
         <div className="myQuotation">
+          {quoteLoading && <img src={Image} alt="Loading..." className="images" />}
           <p className="quotes">{myQuotation}</p>
           <p className="authorQuotes">{authorOfquote}</p>
           {quoteError && <p className="error">{quoteError}</p>}
